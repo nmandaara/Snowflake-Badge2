8d871d0d3f86b6fc4afcec6ad3540dfece829d08
Nikhil M <36548930+nmandaara@users.noreply.github.com>
2022-06-21 22:49:51 -0400

    Update streamlit_app.py
    added few options to select fruity

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #Display the table on page
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+#streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
+
